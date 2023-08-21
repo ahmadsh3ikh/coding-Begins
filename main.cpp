2cda03f0932f540e27ed44fc7ac2a28b25443fae
Ahmad Sheikh <ahmdsh747@gmail.com>
2023-08-22 00:11:41 +0500

    Removed if Condition

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main() {
     for (int age = 15; age < 18; age++) {
         cout << "turning to " << age << endl;
-        if (age == 17) {
-            age++;
-            cout << "turned to " << age;
-        }
+       
     }
 
 
